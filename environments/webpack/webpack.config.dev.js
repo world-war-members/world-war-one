a1dbf2cba2c7f8d577d8fd3d6e820ac5e8e9e794
atakan.tekoglu <atakan.tekoglu@param.com.tr>
2023-04-23 02:23:35 +0300

    fix: clear unnecassary code lines

--- environments/webpack/webpack.config.dev.js
+++ environments/webpack/webpack.config.dev.js
@@ -3,7 +3,7 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 
-const webpack = require("webpack");
+// const webpack = require("webpack");
 
 module.exports = merge(common, {
 	mode: "development",
--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -7,5 +7,5 @@ export const apiSlice = createApi({
 		baseUrl: testConfig.apiUrl,
 	}),
 	tagTypes: ["QUESTIONS"],
-	endpoints: (builder) => ({}),
+	endpoints: () => ({}),
 });
