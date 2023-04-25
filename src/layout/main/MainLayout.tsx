7969abefc7d91c4900392a304b0facb448319ae8
atakan.tekoglu <atakan.tekoglu@param.com.tr>
2023-04-26 01:06:24 +0300

    improve: change text color based on theme

--- src/layout/main/MainLayout.tsx
+++ src/layout/main/MainLayout.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import { Layout } from "antd";
 import MainHeader from "@/components/Layout/Main/Header";
 import MainContent from "@/components/Layout/Main/Content";
+import { useAppSelector } from "@/hooks/useStore";
 
 const MainLayout: React.FC = () => {
+	
+	const { lightMode } = useAppSelector((store)=> store.system);
+
 	return (
-		<Layout style={{ height: "100vh" }}>
+		<Layout 
+			className={`wwo-layout ${!lightMode ? "light-text-color" : "dark-text-color"}`}
+		>
 			<MainHeader />
 			<MainContent />
 		</Layout>
